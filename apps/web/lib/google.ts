dc0da4729d95e3d9b71aea1d8baaf09693db4150
vekariaayush04 <ayushrvekaria@gmail.com>
2025-03-14 11:48:41 +0530

    minor changes

--- apps/web/lib/google.ts
+++ apps/web/lib/google.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
 	process.env.AUTH_GOOGLE_ID!,
 	process.env.AUTH_GOOGLE_SECRET!,
-	"http://localhost:3000/api/login/google/callback"
+	`${process.env.BASE_URL}/api/login/google/callback`
 );
